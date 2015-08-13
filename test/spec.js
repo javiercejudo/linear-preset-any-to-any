fa82135cbd49fa124f2823271b8837c781089214
Javier Cejudo <javier@javiercejudo.com>
2015-08-14 00:38:06 +1000

    refactor(spec): simplify tests

--- test/spec.js
+++ test/spec.js
@@ -4,15 +4,15 @@
 
 require('should');
 
-var anyToAny = require('../src/linear-preset-any-to-any')(require('floating-adapter'));
-var lc = require('linear-converter')(require('floating-adapter'));
+var adapter = require('floating-adapter');
+var anyToAny = require('../src/linear-preset-any-to-any')(adapter);
 var temp = require('linear-presets-temperature');
 
 describe('convert units any to any', function() {
   it('should return presets for any to any', function() {
-    lc.convert(300, anyToAny(temp, 'kelvin', 'fahrenheit')).should.be.exactly(80.33000000000004);
-    lc.convert(40, anyToAny(temp, 'celsius', 'fahrenheit')).should.be.exactly(104);
-    lc.convert(104, anyToAny(temp, 'fahrenheit', 'celsius')).should.be.exactly(40);
-    lc.convert(10, anyToAny(temp, 'celsius', 'celsius')).should.be.exactly(10);
+    anyToAny(temp, 'kelvin', 'fahrenheit').should.eql([[273.15, 373.15], [32, 212]]);
+    anyToAny(temp, 'celsius', 'fahrenheit').should.eql([[0, 100], [32, 212]]);
+    anyToAny(temp, 'fahrenheit', 'celsius').should.eql([[32, 212], [0, 100]]);
+    anyToAny(temp, 'celsius', 'celsius').should.eql([[0, 1], [0, 1]]);
   });
 });
